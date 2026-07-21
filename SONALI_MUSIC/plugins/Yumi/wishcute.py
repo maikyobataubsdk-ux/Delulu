6acdb451bcd2e2c1fd3b1187fe936acf1136304f
huu680222-oss <huu680222@gmail.com>
2026-07-21 20:19:24 +0530

    Update wishcute.py

--- SONALI_MUSIC/plugins/Yumi/wishcute.py
+++ SONALI_MUSIC/plugins/Yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from SONALI_MUSIC import app 
 
-SUPPORT_CHAT = "fictionalgirlie"
+SUPPORT_CHAT = "narzofamily"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
